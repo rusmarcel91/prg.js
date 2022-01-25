a2e89d92d5bcd6f29a776e7ef75af40393825e4a
rusmarcel91 <rus.marcel91@gmail.com>
2022-01-25 21:53:08 +0200

    first commit

---
+++ program.js
@@ -0,0 +1,11 @@
+
+function calcVol() {
+    var raza = document.getElementById("raza").value;
+    var inaltime = document.getElementById("inaltime").value;
+    var PI = 3.14;
+    
+   var sum = parseInt(raza) * parseInt(inaltime) * PI;
+   alert(sum);
+}
+
+
